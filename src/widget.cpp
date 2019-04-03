408a297b544e48903dabd78f73ab16576e81607b
Andrey Yaromenok <ayaromenok@gmail.com>
2019-04-03 16:46:42 +0200

    #8 try to output IR data via cv::Mat

--- src/widget.cpp
+++ src/widget.cpp
@@ -246,8 +246,20 @@ void
 Widget::cvIRUpdate()
 {
     qDebug() << __PRETTY_FUNCTION__;
+
     ir_->getData(data);
+    cv::Mat imgIn(8, 8, CV_8UC1, data.data());
+
+    QImage imageOut(imgIn.cols, imgIn.rows,  QImage::Format_RGB888);
+    cv::Mat imageCvOut(cv::Size(imgIn.cols,imgIn.rows),
+                       CV_8UC3, imageOut.bits());
+
+
+    cv::cvtColor(imgIn, imageCvOut, cv::COLOR_BGR2RGB);
+
+    lbIR_->setPixmap(QPixmap::fromImage(imageOut.scaledToWidth(320)));
     qDebug() << data;
+
 }
 
 void
